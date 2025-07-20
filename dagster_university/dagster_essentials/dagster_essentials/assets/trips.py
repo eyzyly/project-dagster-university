8dbdfef05676f7111af20dafb7f5613c9245f9ee
Eddy Z <74501266+eyzyly@users.noreply.github.com>
2025-07-20 22:06:17 +0000

    add partitions and update trips

--- dagster_university/dagster_essentials/dagster_essentials/assets/trips.py
+++ dagster_university/dagster_essentials/dagster_essentials/assets/trips.py
@@ -5,18 +5,26 @@ import dagster as dg
 from dagster_duckdb import DuckDBResource
 import os
 
-@dg.asset
-def taxi_trips_file() -> None:
-    """
-      The raw parquet files for the taxi trips dataset. Sourced from the NYC Open Data portal.
-    """
-    month_to_fetch = '2023-03'
-    raw_trips = requests.get(
-        f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{month_to_fetch}.parquet"
-    )
+from dagster_essentials.partitions import monthly_partition
 
-    with open(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), "wb") as output_file:
-        output_file.write(raw_trips.content)
+
+@dg.asset(
+        partitions_def=monthly_partition
+)
+def taxi_trips_file(context: dg.AssetExecutionContext) -> None:
+  """
+      The raw parquet files for the taxi trips dataset. Sourced from the NYC Open Data portal.
+  """
+
+  partition_date_str = context.partition_key
+  month_to_fetch = partition_date_str[:-3]
+
+  raw_trips = requests.get(
+      f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{month_to_fetch}.parquet"
+  )
+
+  with open(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), "wb") as output_file:
+      output_file.write(raw_trips.content)
 
 @dg.asset
 def taxi_zones_file() -> None:
---
+++ dagster_university/dagster_essentials/dagster_essentials/partitions.py
@@ -0,0 +1,10 @@
+import dagster as dg
+from dagster_essentials.assets import constants
+
+start_date = constants.START_DATE
+end_date = constants.END_DATE
+
+monthly_partition = dg.MonthlyPartitionsDefinition(
+    start_date=start_date,
+    end_date=end_date
+)
